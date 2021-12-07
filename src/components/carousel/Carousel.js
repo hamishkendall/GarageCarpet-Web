de02d26b69fbb746554a4aaafc92bcab69ea2ffc
Hamish <37350715+hamishkendall@users.noreply.github.com>
2021-12-07 19:35:06 +1300

    Created Carousel and Navbar

---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { CSSTransition, SwitchTransition, TransitionGroup } from 'react-transition-group';
+import './Carousel.css';
+
+function Carousel() {
+
+    const images = require('../../data/CarouselImages.json');
+
+    const [imgIndex, setImgIndex] = useState(0);
+    const [imgSlide, setImgSlide] = useState(images[0]);
+
+    function updateSlide(index){
+        setImgSlide(images[index]);
+    }
+
+    function nextImage(){
+        if(imgIndex >= images.length - 1){
+            setImgIndex(0);
+            updateSlide(0);
+        }
+        else {
+            setImgIndex(prevIndex => prevIndex+1);
+            updateSlide(imgIndex+1);
+        }        
+    }
+
+    function previousImage(){
+        if(imgIndex === 0){
+            setImgIndex(images.length - 1);
+            updateSlide(images.length - 1);
+        }
+        else {
+            setImgIndex(prevIndex => prevIndex-1);
+            updateSlide(imgIndex-1);
+        }
+    }
+
+    return(
+        <div className='carousel-container'>
+            <div className="number">{imgIndex+1} / {images.length}</div>
+
+            <SwitchTransition>
+                <CSSTransition key={imgSlide.id} timeout={500} classNames="item">
+                    <img className="image" src={imgSlide.imageUrl} alt="" />
+                </CSSTransition>
+            </SwitchTransition>
+
+
+            <div className="text">{imgSlide.text}</div>
+
+            <a className="prev" onClick={previousImage}>&#10094;</a>
+            <a className="next" onClick={nextImage}>&#10095;</a>
+        </div>
+    );
+
+}
+
+export default Carousel;
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css';
+
+function NavBar(props) {
+
+
+    return (
+        <div className='NavBar'>
+            <img src="https://i.imgur.com/YryIGQy.png" />
+        </div>
+    );
+}
+
+export default NavBar;
